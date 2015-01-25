90d228618764e5721643056bf4cde2c170427a72
vmedvedev <fknd99@gmail.com>
2015-01-25 18:54:24 +0600

    Assigment 2 submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions perform calculations of matrix inverse with ability to cache results and 
+## checking if matrix is inversible
 
-## Write a short comment describing this function
+## Function makeCacheMatrix accept matrix as an argument
+## and constructs expose list of methods to get and set cached value
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inv) inverse <<- inv
+    getinverse <- function() inverse
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
-
-
-## Write a short comment describing this function
+    
+## Accepts matrix constructed by makeCacheMatrix, calculates and returns inverse matrix,
+## of returns cached result, also check if matrix is inversible
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    if (det(data) == 0) {
+        message("determinant equals zero, the matrix is singular")
+        return(NA)
+    }
+    inv <- solve(data, ...)
+    x$setinverse(inv)
+    inv
 }
